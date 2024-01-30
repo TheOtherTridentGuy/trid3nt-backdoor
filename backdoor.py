30a17cff67f660a599b9846cab8f9518d95b8156
TheOtherTridentGuy <aibohlander@s.sfusd.edu>
2024-01-29 17:32:07 -0800

    client_id

--- backdoor.py
+++ backdoor.py
@@ -1,5 +1,10 @@
 # pylint: disable-all
+import secrets
 import json
 
 
 config = json.load(open("config.json", "r+"))
+if config.get("client_id"):
+    client_id = config.get("client_id")
+else:  
+    client_id = secrets.token_urlsafe(256)
